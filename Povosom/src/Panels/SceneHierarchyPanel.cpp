b49a38ef508cb03c6c40f3d3640f55a78ec3314b
PowerOfNames <PowerOfNames@web.de>
2021-04-30 16:41:40 +0200

    added PropertiesPanel

--- Povosom/src/Panels/SceneHierarchyPanel.cpp
+++ Povosom/src/Panels/SceneHierarchyPanel.cpp
@@ -2,6 +2,7 @@
 
 #include "Povox/Scene/Components.h"
 
+#include <glm/gtc/type_ptr.hpp>
 #include <ImGui/imgui.h>
 
 namespace Povox {
@@ -30,6 +31,19 @@ namespace Povox {
 			ImGui::TreePop();
 		}
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+		{
+			m_SelectionContext = {};
+		}
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+
+		if (m_SelectionContext)
+		{
+			DrawComponents(m_SelectionContext);
+		}
+
 		ImGui::End();
 	}
 
@@ -46,10 +60,43 @@ namespace Povox {
 
 		if (opened)
 		{
-			//ImGui::InputTextWithHint("%s", tag.c_str(), );
 			ImGui::Text("%s", tag.c_str());
 			ImGui::TreePop();
 		}
 
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+
+			ImGui::Text("Name");
+			ImGui::SameLine();
+			if (ImGui::InputTextWithHint("", tag.c_str(), buffer, IM_ARRAYSIZE(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+			ImGui::Separator();
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+
+				ImGui::Text("Position");
+				ImGui::SameLine();
+				ImGui::DragFloat3("", glm::value_ptr(transform[3]), 0.5);
+
+
+				ImGui::Separator();
+				ImGui::TreePop();	
+			}
+		}
 	}
 }
