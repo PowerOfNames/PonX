373d9021da7e650763d19aaa7ad014b3d7f97172
PowerOfNames <PowerOfNames@web.de>
2021-05-01 13:20:12 +0200

    added CameraComponentUI

--- Povosom/src/Panels/SceneHierarchyPanel.cpp
+++ Povosom/src/Panels/SceneHierarchyPanel.cpp
@@ -98,5 +98,72 @@ namespace Povox {
 				ImGui::TreePop();	
 			}
 		}
+
+		if (entity.HasComponent<CameraComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_DefaultOpen, "Camera"))
+			{
+				auto& cameraComp = entity.GetComponent<CameraComponent>();
+				auto& camera = cameraComp.Camera;
+
+				const char* projectionTypeStrings[] = { "Perspective", "Orthographic" };
+				const char* currentProjectionTypeString = projectionTypeStrings[(int)camera.GetProjectionType()];
+
+				//TODO: Needs to set all other cameras to not being primary!
+				ImGui::Checkbox("Primary", &cameraComp.Primary);
+
+				if (ImGui::BeginCombo("Projection", currentProjectionTypeString))
+				{
+					for (int i = 0; i < 2; i++)
+					{
+						bool isSelected = currentProjectionTypeString == projectionTypeStrings[i];
+						if (ImGui::Selectable(projectionTypeStrings[i], isSelected))
+						{
+							currentProjectionTypeString = projectionTypeStrings[i];
+							camera.SetProjectionType((SceneCamera::ProjectionType)i);
+						}
+
+						if (isSelected)
+							ImGui::SetItemDefaultFocus();
+					}
+					ImGui::EndCombo();
+				}
+
+				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Perspective)
+				{
+					float verticalFOV = glm::degrees(camera.GetPerspectiveVerticalFOV());
+					if (ImGui::DragFloat("Vertical FOV", &verticalFOV))
+						camera.SetOrthographicSize(glm::radians(verticalFOV));
+
+					float perspectiveNearClip = camera.GetPerspectiveNearClip();
+					if (ImGui::DragFloat("Near Clip", &perspectiveNearClip))
+						camera.SetPerspectiveNearClip(perspectiveNearClip);
+
+					float perspectiveFarClip = camera.GetPerspectiveFarClip();
+					if (ImGui::DragFloat("Far Clip", &perspectiveFarClip))
+						camera.SetPerspectiveFarClip(perspectiveFarClip);
+				}
+
+				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Orthographic)
+				{
+					float orthoSize = camera.GetOrthographicSize();
+					if (ImGui::DragFloat("Size", &orthoSize))
+						camera.SetOrthographicSize(orthoSize);
+
+					float orthoNearClip = camera.GetOrthographicNearClip();
+					if (ImGui::DragFloat("Near Clip", &orthoNearClip))
+						camera.SetOrthographicNearClip(orthoNearClip);
+
+					float orthoFarClip = camera.GetOrthographicFarClip();
+					if (ImGui::DragFloat("Far Clip", &orthoFarClip))
+						camera.SetOrthographicFarClip(orthoFarClip);
+
+					ImGui::Checkbox("Fixed Aspect Ratio", &cameraComp.FixedAspectRatio);
+				}
+
+				ImGui::Separator();
+				ImGui::TreePop();
+			}
+		}
 	}
 }
--- Povox/src/Povox/Scene/SceneCamera.cpp
+++ Povox/src/Povox/Scene/SceneCamera.cpp
@@ -14,8 +14,22 @@ namespace Povox {
 	{
 	}
 
+	void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
+	{
+		m_ProjectionType = SceneCamera::ProjectionType::Perspective;
+
+		m_PerspectiveVerticalFOV = verticalFOV;
+		m_PerspectiveNear = nearClip;
+		m_PerspectiveFar = farClip;
+
+		RecalculateProjection();
+
+	}
+
 	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
 	{
+		m_ProjectionType = SceneCamera::ProjectionType::Orthographic;
+
 		m_OrthographicSize = size;
 		m_OrthographicNear = nearClip;
 		m_OrthographicFar = farClip;
@@ -31,11 +45,19 @@ namespace Povox {
 
 	void SceneCamera::RecalculateProjection()
 	{
-		float orthoLeft = -m_OrthographicSize * 0.5f * m_AspectRatio;
-		float orthoRight = m_OrthographicSize * 0.5f * m_AspectRatio;
-		float orthoBottom = -m_OrthographicSize * 0.5f;
-		float orthoTop = m_OrthographicSize * 0.5f;
 
-		m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+		if (m_ProjectionType == SceneCamera::ProjectionType::Perspective)
+		{
+			m_Projection = glm::perspective(m_PerspectiveVerticalFOV, m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+		}
+		else
+		{
+			float orthoLeft = -m_OrthographicSize * 0.5f * m_AspectRatio;
+			float orthoRight = m_OrthographicSize * 0.5f * m_AspectRatio;
+			float orthoBottom = -m_OrthographicSize * 0.5f;
+			float orthoTop = m_OrthographicSize * 0.5f;
+
+			m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+		}
 	}
 }
